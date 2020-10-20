f9cbfad68baa5e55994fce056fa4c49100de3290
ZyCromerZ <neetroid97@gmail.com>
2020-10-20 23:06:57 +0700

    add function to create libs using drone ci
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- getlib.sh
+++ getlib.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+[[ -z "$LINK" ]] && LINK="https://raw.githubusercontent.com/ZyCromerZ/redmi_begonia_dump/$branch"
 
-LINK="https://raw.githubusercontent.com/ZyCromerZ/redmi_begonia_dump/$branch"
+GetLibPath="$(pwd)"
 
 generateLib(){
-    rm -rf ./system//vendor/lib/egl/*
-    rm -rf ./system//vendor/lib64/egl/*
+    rm -rf $GetLibPath/system/vendor/lib/egl/*
+    rm -rf $GetLibPath/system/vendor/lib64/egl/*
+    [ ! -d $GetLibPath/system ] && mkdir $GetLibPath/system
+    [ ! -d $GetLibPath/system/vendor ] && mkdir $GetLibPath/system/vendor
+    [ ! -d $GetLibPath/system/vendor/lib ] && mkdir $GetLibPath/system/vendor/lib
+    [ ! -d $GetLibPath/system/vendor/lib64 ] && mkdir $GetLibPath/system/vendor/lib64
+    [ ! -d $GetLibPath/system/vendor/lib/egl ] && mkdir $GetLibPath/system/vendor/lib/egl
+    [ ! -d $GetLibPath/system/vendor/lib64/egl ] && mkdir $GetLibPath/system/vendor/lib64/egl
     getLib "$1" "vendor/lib/egl/egl.cfg"
     getLib "$1" "vendor/lib/egl/libGLES_mali.so"
     getLib "$1" "vendor/lib/egl/libGLES_meow.so"
@@ -15,20 +23,26 @@ generateLib(){
 }
 
 getLib(){
-    wget "$1"/$2 -O ./system/$2
-    FileSize="$(wc -c "system/$2" | awk '{print $1}')"
-    [ "$FileSize" == '0' ] && rm -rf ./system/$2
+    link="$1"
+    path="$2"
+    wget "$link/$path" -O "$GetLibPath/system/$path"
 }
 
 generateLib "$LINK"
 Version="$(date +'%m%d%Y')"
 VersionCode="$(date +'%s')"
-sed -i "s/00000/$Version/g" ./module.prop 
-sed -i "s/11111/$VersionCode/g" ./module.prop 
-sed -i "s/22222/$ZipName/g" ./module.prop
+sed -i "s/00000/$Version/g" "$GetLibPath/module.prop" 
+sed -i "s/11111/$VersionCode/g" "$GetLibPath/module.prop" 
+sed -i "s/22222/$ZipName/g" "$GetLibPath/module.prop"
 
-GpuZipName="$(pwd)/Mali.GPU.update-downdate.from.$ZipName"
+GpuZipName="$GetLibPath/Mali.GPU.update-downdate.from.$ZipName"
 
-zip -r9 "$GpuZipName" * -x .git README.md .gitignore *.zip getlib.sh
+rm -rf compilled-gdrive
+zip -r9 "$GpuZipName" * -x ./.git ./README.md ./.gitignore ./*.zip ./getlib.sh ./run.sh
+
+[[ ! -d compilled-gdrive ]] && git clone https://${GIT_SECRET}@github.com/ZyCromerZ/gdrive_uploader.git compilled-gdrive
+cd compilled-gdrive
+chmod +x run.sh
+. run.sh "$GpuZipName" doc "$(date +'%m-%d-%Y')"
 
 git reset --hard
