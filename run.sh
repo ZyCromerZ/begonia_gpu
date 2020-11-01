4ae67d93771bc18581391ed82fd592a50302822a
ZyCromerZ <neetroid97@gmail.com>
2020-11-01 23:40:36 +0700

    run.sh: update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -40,4 +40,6 @@ MakeGpuModule miui_BEGONIA_20.10.22_ec8abec846_10.0.zip begonia-user-10-QP1A.190
 
 MakeGpuModule miui_BEGONIA_20.10.29_04b140f215_10.0.zip begonia-user-10-QP1A.190711.020-20.10.29-release-keys
 
+MakeGpuModule miui_BEGONIA_20.10.30_c34625a5ce_10.0.zip begonia-user-10-QP1A.190711.020-20.10.30-release-keys
+
 cd "$currPath" && rm -rf "$currPath/*" 
