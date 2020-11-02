600725647593bcf30748639cb9d37c235026b083
ZyCromerZ <neetroid97@gmail.com>
2020-11-02 18:20:20 +0700

    build modules from:: miui_BEGONIA_20.11.2_d7f8e06908_10.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ MakeGpuModule(){
     cd $currPath
 }
 
-MakeGpuModule miui_BEGONIA_20.10.30_c34625a5ce_10.0.zip begonia-user-10-QP1A.190711.020-20.10.30-release-keys
+MakeGpuModule miui_BEGONIA_20.11.2_d7f8e06908_10.0.zip begonia-user-10-QP1A.190711.020-20.11.2-release-keys
 
 cd "$currPath" && rm -rf "$currPath/*" 
