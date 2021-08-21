706c54c5cd7004a0f6a84b84441d34fba3937a92
ZyCromerZ <neetroid97@gmail.com>
2021-08-21 14:53:45 +0700

    update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ MakeGpuModule(){
     cd $currPath
 }
 
-MakeGpuModule miui_BEGONIA_21.1.22_d28f4ee734_11.0.zip begonia-user-11-RP1A.200720.011-21.1.22-release-keys
+MakeGpuModule miui_BEGONIAIDGlobal_V12.5.2.0.RGGIDXM_c690ecd789_11.0.zip begonia-user-11-RP1A.200720.011-V12.5.2.0.RGGIDXM-release-keys
 
 cd "$currPath" && rm -rf "$currPath/*" 
